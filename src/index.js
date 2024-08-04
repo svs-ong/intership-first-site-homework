23015d4ead2fc32031b08f4e5ef04ef599e713ab
zaOdette <iugahoriaodette@gmail.com>
2024-08-04 15:37:54 +0300

    homework

---
+++ src/index.js
@@ -0,0 +1,26 @@
+async function getElementFromFile(filePath, elementId) {
+    try {
+      // Use Fetch API to get the element content
+      // elementAsString is a string containing the HTML content of the element
+      const elementAsString = await (await fetch(filePath)).text();
+  
+      // Parse the elementAsString into a Document Object Model (DOM)
+      const parser = new DOMParser();
+      const parsedElement = parser.parseFromString(elementAsString, "text/html");
+  
+      return parsedElement.getElementById(elementId);
+    } catch (error) {
+      console.error(`Failed to load element from file: ${filePath}`, error);
+      return null;
+    }
+}
+
+async function loadContent() {
+    const header = await getElementFromFile('/components/header.html', 'header');
+    const footer = await getElementFromFile('/components/footer.html', 'footer');
+
+    document.getElementById('header').appendChild(header);
+    document.getElementById('footer').appendChild(footer);
+}
+
+window.onload = loadContent;
