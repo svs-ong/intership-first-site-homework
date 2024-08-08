7e1ce6a32247096339b691b70a9871ced316e4c9
Kimin <mihailchivu8@gmail.com>
2024-08-09 02:30:09 +0300

    Working article page

--- homework/components/article-page-logic/scripts/script.js
+++ homework/components/article-page-logic/scripts/script.js
@@ -1,13 +1,13 @@
-import { fetch_articles } from '../../fetch_articles.js';
+import { fetch_articles } from '../../../api/fetch_articles.js';
 
 document.addEventListener("DOMContentLoaded", main);
 
 async function main() {
     console.log('Main function executed');
     try {
-        const articlesContainer = document.getElementById("articles-container");
-        const templateSource = document.getElementById("article-template").innerHTML;
-        const template = Handlebars.compile(templateSource);
+        const articlesContainer = document.getElementById("articles-container"); //Get the article container element
+        const templateSource = document.getElementById("article-template").innerHTML; //Get the template source
+        const template = Handlebars.compile(templateSource);//Compile the Handlebars template
         const articles = await fetch_articles();
         const articleIndex = 0; // Change this to the index of the article you want to render
         const article = articles[articleIndex];
