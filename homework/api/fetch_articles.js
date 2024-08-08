c595126a37cd9fc409e572f60c92150d36123ff1
Axetyy <iuliannistor06@gmail.com>
2024-08-08 16:04:46 +0300

    final tests for fetch_articles

--- homework/api/fetch_articles.js
+++ homework/api/fetch_articles.js
@@ -20,12 +20,14 @@
 
 export async function fetch_articles() {
     console.log("Fetching articles...");
+    let arr=[];
     try {
         const response = await fetch('http://localhost:3000/articles');
-        const data = await response.json();
+        arr = await response.json();
         console.log("Data fetched successfully");
-        console.log( data);
+        return arr;
     } catch (error) {
         console.error("Error fetching data", error);
+        return [];
     }
 }
--- homework/api/test/test.js
+++ homework/api/test/test.js
@@ -1,7 +1,34 @@
-import {fetch_articles} from '../fetch_articles.js';
+import { fetch_articles } from '../fetch_articles.js';
 
 document.addEventListener("DOMContentLoaded", main);
 
-function main(){
-    fetch_articles();
+// async function main() {
+//     getArticlesArray();
+//   
+//     articles.forEach(article => {
+//     });
+// }
+async  function main()
+{
+    let articles=await fetch_articles();
+    const articlesContainer = document.getElementById("articles-container");
+    for(let article of articles)
+    {
+        let div=createDiv(article);
+        articlesContainer.appendChild(div);
+    }
+}
+
+function createDiv(article)
+{
+        const articleDiv = document.createElement("div"); // creates a new div
+
+        articleDiv.classList.add("article"); // adds the class name "article " to the created div
+
+        const titleElement = document.createElement("h2"); // creates a <h2> element
+
+        titleElement.innerText=article.title; // appends the article.title  inside the h2
+
+        articleDiv.appendChild(titleElement); // appends the title to the div
+        return articleDiv; //returns the div to be later appended in the main function
 }
