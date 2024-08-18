2dad32ba7b8fe27e248534b21de5f50961c74666
Axetyy <iuliannistor06@gmail.com>
2024-08-18 18:26:16 +0300

    footer styling

--- react-homework/src/App.tsx
+++ react-homework/src/App.tsx
@@ -4,36 +4,40 @@ import { Nav } from './components/Nav';
 import { HomePageFunction } from './pages/Home/HomePage';
 import { LogoPageFunction } from './pages/Logo/LogoPage';
 import { ContactPageFunction } from './pages/Contact/ContactPage';
-import Footer from './components/Footer';
+import { Footer } from './components/Footer';
 import { FeaturedArticles } from './components/FeaturedArticles';
 import { SearchPage } from './pages/Search/SearchPage';
+import './styles/App.styles.css';
 
-
-const ShowFeaturedArticles:React.FC=()=>{
-  const location=useLocation();  //uselocation function
-  if(location.pathname=== '/' || location.pathname==='/home')  // shows the Featured Articles only if its on the home page
+const ShowFeaturedArticles: React.FC = () => {
+  const location = useLocation();  //uselocation function
+  if (location.pathname === '/' || location.pathname === '/home')  // shows the Featured Articles only if its on the home page
     return <FeaturedArticles date={``}></FeaturedArticles>
-  else 
+  else
     return null;  //else not needed but just in case
 }
 
 const App: React.FC = () => {
   return (
-    <><Router>
-      <Nav />
-      <Routes>
-        <Route path="/logo" element={<LogoPageFunction />} />
-        <Route path="/contact" element={<ContactPageFunction />} />
-        <Route path="/home" element={<HomePageFunction />} />
-        <Route path="/search" element={<SearchPage/>}/>
-        <Route path="/" element={<HomePageFunction />} /> {/* Default route to Home */}
-      </Routes>
-      <ShowFeaturedArticles/>
-    <Footer/>
-    </Router>
-    
-    
-    </>
+    <div className='wrapper'>
+        <><Router>
+          <Nav />
+          <div className='content'>
+          <Routes>
+            <Route path="/logo" element={<LogoPageFunction />} />
+            <Route path="/contact" element={<ContactPageFunction />} />
+            <Route path="/home" element={<HomePageFunction />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/" element={<HomePageFunction />} /> {/* Default route to Home */}
+          </Routes>
+          
+          <ShowFeaturedArticles />
+          </div>
+          <Footer />
+        </Router>
+        </>
+      
+    </div>
   );
 };
 
--- react-homework/src/components/Footer.tsx
+++ react-homework/src/components/Footer.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-const Footer: React.FC=()=>
-{
+import "../styles/Footer.styles.css";
+export const Footer: React.FC = () => {
     return (
-<footer className="Footer" style={{alignContent:"center"}}>
-    &copy; Rajat Rawal
-</footer>
+            <footer className="footer">
+                &copy; Rajat Rawal
+            </footer>
     );
 }
-export default Footer;
