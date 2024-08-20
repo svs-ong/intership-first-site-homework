89f26a1504b70fe3583d65fbe5ee5339cd77c017
Kimin <mihailchivu8@gmail.com>
2024-08-20 14:56:38 +0300

    hero_section

---
+++ react-homework/src/components/hero_section/hero_section.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './hero_section.styles.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="Title">Title</h1>
+        <p className="description">kknkgjpkdjpkjksjpkjlkpoknpkj[ljpiokpo[lojoiopepikjkjsdffffffkffffffffffffffffffffffffffffffffffffff</p>
+      </div>
+      <div className="image-container">
+        <img src="image_URL" alt="Hero Image" className="image" /> 
+        <div className="circle" />
+      </div>
+    </div>
+  );
+}
+export default Hero;
